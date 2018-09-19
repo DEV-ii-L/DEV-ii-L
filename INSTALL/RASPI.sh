a30ba17fcd16e7b2c0b0ec5b730e5d2613d7797c
DEV-ii-L <42357821+DEV-ii-L@users.noreply.github.com>
2018-09-19 17:44:36 +0200

    Update RASPI.sh

--- INSTALL/RASPI.sh
+++ INSTALL/RASPI.sh
@@ -6,12 +6,15 @@ echo "[+] Installing $HEADER tools suite..."
 
 mkdir -p /opt/$HEADER/
 cd /opt/$HEADER/
+wget https://bitbucket.org/dani_thomas/rf433ctrl/get/4a63942803e6.zip -o rf433ctrl.zip
 ##################################################################################
 toollist=(
 
 'https://github.com/ChristopheJacquet/PiFmRds.git'
 'https://github.com/F5OEO/rpitx.git'
-
+'https://github.com/lthiery/SPI-Py.git'
+'https://github.com/mxgxw/MFRC522-python.git'
+'https://github.com/WiringPi/WiringPi.git'
 
 )
 ##################################################################################
